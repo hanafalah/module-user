d59efa004b240cfa250a05356aee3705bfab03ac
hamzah <hamzahnuralfalah@gmail.com>
2025-03-24 05:35:37 +0700

    update user config

--- assets/config/config.php
+++ assets/config/config.php
@@ -10,21 +10,19 @@ return [
     'app' => [
         'contracts' => [
             //ADD YOUR CONTRACTS HERE
-            'user'           => Contracts\User::class,
-            'user_reference' => Contracts\UserReference::class
         ],
     ],
     'commands' => [
         ModuleUserCommands\InstallMakeCommand::class
     ],
     'libs' => [
-        'model' => 'Models',
-        'contract' => 'Contracts'
+        'model'    => 'Models',
+        'contract' => 'Contracts',
+        'schema'   => 'Schemas'
     ],
     'database' => [
         'models' => [
-            // 'User'           => ModuleUserModels\User::class,
-            // 'UserReference'  => ModuleUserModels\UserReference::class
+            //ADD YOUR MODELS HERE
         ]
     ]
 ];
